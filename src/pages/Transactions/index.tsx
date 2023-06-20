4b2a5c65b604d29b83c7fb6550ba9465a5b51cc3
unknown <gabriel.evanger@gmail.com>
2023-06-20 18:09:20 -0300

    Finalizado Css e tela de cadastro
    realizando requisições HTTP
    
    Signed-off-by: unknown <gabriel.evanger@gmail.com>

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
+ useEffect(() => {
+  fetch('http://localhost:3333/transactions')
+      .then(response => {
+        response.text().then(data => {
+          console.log(data)
+        })
+  })
+ },[])
+
   return (
     <div>
       <Header />
